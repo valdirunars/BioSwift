ae45f835a17cc52de088717d8946bcc4ca33066f
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-09-27 21:05:27 +0000

    Make Genome conform to simpleencodable

--- Sources/Genome+SimpleEncodable.swift
+++ Sources/Genome+SimpleEncodable.swift
@@ -8,23 +8,11 @@
 import Foundation
 
 protocol SimpleEncodable {
-    associatedtype S: Sequence where S.Element == Byte
-    var bytes: S { get }
     func encode() -> Data
 }
 
-extension SimpleEncodable {
-    func encode() -> Data {
-        if let encoded = self.bytes as? Data {
-            return encoded
-        } else {
-            return Data(self.bytes)
-        }
-    }
-}
-
 extension Genome: SimpleEncodable {
-    var bytes: [Byte] {
-        return self.map { $0.rawValue }
+    func encode() -> Data {
+        return self.asInteger().description.data(using: .utf8)!
     }
 }
