02dd01951c5141e8f95a75773b5bbd52a1269a1d
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-10-11 01:35:28 +0000

    Remove ssl package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["BioSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/valdirunars/BigIntCompress.git", from: "1.0.0"),
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.0")
+        .package(url: "http://github.com/valdirunars/BigIntCompress.git", from: "1.0.0"),
+        .package(url: "http://github.com/attaswift/BigInt.git", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
